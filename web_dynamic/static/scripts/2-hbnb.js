8e69cbbbcdb985d0900e35e47b7d3817a40a5056
SimonDesc <pro.descombes@gmail.com>
2023-09-25 14:55:53 +0200

    fix: remove the console.log

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -3,7 +3,6 @@
 $.get('http://127.0.0.1:5001/api/v1/status/', function (data) {
   console.log(data.status);
   if (data.status === 'OK') {
-    console.log('prout');
     $('div#api_status').addClass('available');
   } else {
     $('div#api_status').removeClass('available');
