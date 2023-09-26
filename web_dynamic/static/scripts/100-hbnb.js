f17d06061d9ae7f5a68c32fc5eac977619dfc38e
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-09-26 02:43:42 -0700

    create task requiered files and try to have a whole functional filter

---
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -0,0 +1,122 @@
+$(() => {
+	let amenities = {};
+	const cities = {};
+
+  $('.amenities input[type="checkbox"]').change(function () {
+    const amenityId = $(this).closest('li').data('id'); // Prendre l'ID de l'aménité de l'élément li le plus proche
+    const amenityName = $(this).closest('li').data('name'); // Prendre le nom de l'aménité de l'élément li le plus proche
+
+    if ($(this).is(':checked')) {
+      amenities[amenityId] = amenityName;
+    } else {
+      delete amenities[amenityId];
+    }
+
+	  $('.amenities h4').text(Object.values(amenities).join(', '));
+	  console.log(amenities);
+  });
+	
+  $('.locations ul li ul li input[type="checkbox"]').change(function () {
+	const cityId = $(this).closest('li').data('id');
+	const cityName = $(this).closest('li').data('name');
+    
+	if ($(this).is(':checked')) {
+	  cities[cityId] = cityName;
+	} else {
+	  delete cities[cityName];
+	}
+    
+	      $('.locations h4').text(Object.values(cities).join(', '));
+      });
+
+  $.get('http://127.0.0.1:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+
+	// Fetch all places on the page
+  $.ajax({
+    url: 'http://127.0.0.1:5001/api/v1/places_search',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function (place) {
+      for (let i = 0; i < place.length; i++) {
+        const user_url = 'http://127.0.0.1:5001/api/v1/users/' + place[i].user_id;
+        $.get(user_url, function (owner) {
+          const article = `
+			<article>
+			<div class="title_box">
+				<h2>${place[i].name}</h2>
+				<div class="price_by_night">$${place[i].price_by_night}</div>
+			</div>
+			<div class="information">
+				<div class="max_guest">${place[i].max_guest} Guest${place[i].max_guest > 1 ? 's' : ''}</div>
+				<div class="number_rooms">${place[i].number_rooms} Bedroom${place[i].number_rooms > 1 ? 's' : ''}</div>
+				<div class="number_bathrooms">${place[i].number_bathrooms} Bathroom${place[i].number_bathrooms > 1 ? 's' : ''}</div>
+			</div>
+			<div class="user">
+			<b>Owner:</b> ${owner.first_name} ${owner.last_name}
+			</div>
+			<div class="description">
+				${place[i].description || ''}
+			</div>
+
+			</article>`;
+
+          $('section.places').append(article);
+        });
+      }
+    },
+    error: function (error) {
+      console.error(error);
+    }
+  });
+
+	$('button').on('click', () => {
+		const filters = {
+			amenities: amenities,
+			cities: cities,
+	  }
+    $.ajax({
+      url: 'http://127.0.0.1:5001/api/v1/places_search',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(filters),
+      success: function (place) {
+        for (let i = 0; i < place.length; i++) {
+          const user_url = 'http://127.0.0.1:5001/api/v1/users/' + place[i].user_id;
+          $.get(user_url, function (owner) {
+            const article = `
+				<article>
+				<div class="title_box">
+					<h2>${place[i].name}</h2>
+					<div class="price_by_night">$${place[i].price_by_night}</div>
+				</div>
+				<div class="information">
+					<div class="max_guest">${place[i].max_guest} Guest${place[i].max_guest > 1 ? 's' : ''}</div>
+					<div class="number_rooms">${place[i].number_rooms} Bedroom${place[i].number_rooms > 1 ? 's' : ''}</div>
+					<div class="number_bathrooms">${place[i].number_bathrooms} Bathroom${place[i].number_bathrooms > 1 ? 's' : ''}</div>
+				</div>
+				<div class="user">
+			    <b>Owner:</b> ${owner.first_name} ${owner.last_name}
+			  </div>
+				<div class="description">
+					${place[i].description || ''}
+				</div>
+	
+				</article>`;
+
+            $('section.places').html(article);
+          });
+        }
+      },
+      error: function (error) {
+        console.error(error);
+      }
+    });
+  });
+});
